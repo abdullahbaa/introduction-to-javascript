4f0f510b9b497463f91b27deea8e4411297f9652
abdullahraihan03 <aaziz191231@bscse.uiu.ac.bd>
2024-11-05 16:26:12 +0600

    added all codes

---
+++ practicetask.js
@@ -0,0 +1,32 @@
+
+// Task 1
+var orange =400;
+var apple =300;
+var money =1000;
+var total =orange+apple;
+var back =money - total;
+// console.log(total);
+// console.log(back);
+
+// Task 2
+
+var math = 75.5;
+var biology =65;
+var chemistry =35.45;
+var physics =99.50;
+var total = math + biology + chemistry + physics;
+var gorr =total.toFixed(3) / 4  ;
+console.log(gorr);
+
+// Task 3
+
+var num1 =119;
+var num2=5;
+var div = num1 % num2;
+console.log(div);
+
+// Task 4
+
+const a =isNaN ('11');
+const a =isNaN(2-10);
+console.log(a);
