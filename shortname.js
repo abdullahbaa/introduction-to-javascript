47bbf8748a2188a8a060902fd78fbf97d00e5ddc
abdullahraihan03 <aaziz191231@bscse.uiu.ac.bd>
2024-11-05 15:51:54 +0600

    added all JS codes

---
+++ shortname.js
@@ -0,0 +1,15 @@
+var peice =34;
+price =price +20;
+// price +=20;
+console.log(price);
+
+var fridge=37000;
+fridge =fridge-1000;
+// fridge -=1000;
+console.log(fridge);
+
+
+var orangePrice =30;
+orangePrice =orangePrice *3;
+// orangePrice *= 3;
+console.log(orangePrice)
